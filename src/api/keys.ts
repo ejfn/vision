be863574aed87652291566d471d90bf742437350
Eric Fan <eric.fan@alcidion.com>
2017-07-17 14:01:17 +0930

    better handling gitignores

--- src/api/keys.ts
+++ src/api/keys.ts
@@ -1,5 +1,3 @@
-
-//export const FACE_API_KEY: string = 'xxxxxxxxxxxxxxxx';
-//export const EMOTION_API_KEY: string = 'xxxxxxxxxxxxxxxx';
-//export const VISION_API_KEY: string = 'xxxxxxxxxxxxxxxx';
-
+export const FACE_API_KEY: string = 'xxxxxxxxxxxxxxxx';
+export const EMOTION_API_KEY: string = 'xxxxxxxxxxxxxxxx';
+export const VISION_API_KEY: string = 'xxxxxxxxxxxxxxxx';
