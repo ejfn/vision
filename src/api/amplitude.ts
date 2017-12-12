786f0c008c76fe2ee8cf0af2a7dac6970b694592
Eric Fan <eric.fan@alcidion.com>
2017-12-12 15:42:12 +1030

    revert events change

--- src/api/amplitude.ts
+++ src/api/amplitude.ts
@@ -4,10 +4,10 @@ import { AppMode } from '../store';
 import { AzureLocation } from '../typings/extra';
 
 export function logApiCalledEvent(appMode: AppMode, location: AzureLocation, error?: Error): void {
-  Amplitude.logEventWithProperties('Api Called', {
+  const eventName: string = error === undefined ? `${appMode} Api Succeeded` : `${appMode} Api Failed`;
+  Amplitude.logEventWithProperties(eventName, {
     apiType: appMode,
     azureLocation: location,
-    succeeded: error === undefined,
     error
   });
 }
